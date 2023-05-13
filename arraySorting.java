d44e39bd0ac26a17bcb0cbe44bce80666e4aa963
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-05-13 08:48:15 +0530

    some commit

---
+++ arraySorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class arraySorting {
+    public static void main(String args[]){
+        int[] arr={1,2,4,7,-1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+    
+}
---
+++ removeLeadingTailingSpace.java
@@ -0,0 +1,10 @@
+
+
+public class removeLeadingTailingSpace{
+    public static void main(String args[]) {
+        String s="asd df\bdve";
+        String S = s.strip();
+        System.out.println(S);
+        
+    }
+}

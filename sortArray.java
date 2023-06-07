e0ddbbdea05b5374e5a0402f28fc9130ca143787
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-06-07 07:24:09 +0530

    first commit

---
+++ sortArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String args[]){
+        int[] myArray={1,2,13,14,5};
+         Arrays.sort(myArray);
+         for(int temp : myArray)
+         {
+            System.out.println(temp+"");
+         }
+    }
+    
+}

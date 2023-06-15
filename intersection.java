bf54c42d1c26296204a7bf0470c6985eb6d44731
Krishnaraj S <krishnarajshetty699@gmail.com>
2023-06-15 17:40:40 +0530

    some qns

---
+++ intersection.java
@@ -0,0 +1,41 @@
+
+public class intersection {
+    static int i, j, k, c = 0;
+
+    public static void main(String args[]) {
+        int[] x = { 2, 5, 3, 7 };
+        System.out.println("The first array is" + "");
+        for (int i = 0; i < x.length; i++)
+            System.out.println(x[i]);
+        System.out.println("");
+        int[] y = { 5, 2, 9, 10 };
+        System.out.println("The second array is" + "");
+        for (int j = 0; j < y.length; j++)
+            System.out.println(y[j]);
+        System.out.println("");
+        intersection(x,y);
+
+
+    }
+    public static void intersection(int x[],int y[]){
+        int[] z=new int[x.length+y.length];
+        for(int i=0;i<x.length;i++){
+            for(int j=0;j<y.length;j++){
+                if(x[i]==y[j]){
+                    z[c]=x[i];
+                    c++;
+                }
+                else
+                 continue;
+            }
+        }
+        System.out.println("Intersection of two array is"+"");
+        for(k=0;k<c;k++){
+            System.out.println(""+z[k]+"");
+        }
+        System.out.println("");
+
+        
+    }
+
+}
